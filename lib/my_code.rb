e05efdd5aec8920cf7b9758012e863722cce441d
mystical-calculator-5374 <bbrower@smu.edu>
2020-01-25 03:07:41 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -35,8 +35,8 @@ def map_to_square(source_array)
 end
 
 def reduce_to_total(source_array, starting_point = 0)
-  reduced = 0
-  i = starting_point
+  reduced = starting_point
+  i = 0
   while i < source_array.size
     reduced += source_array[i]
     i += 1
